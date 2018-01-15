e47d452805202109df40e5ff4b33d91134ada4a3
Michael Morris <michael.morris@ruralsource.com>
2018-01-15 10:48:02 -0600

    BrowserStack Integration

--- protractor.conf.js
+++ protractor.conf.js
@@ -3,15 +3,21 @@
 
 const { SpecReporter } = require('jasmine-spec-reporter');
 
+// Browserstack local connection.
+var browserstack = require('browserstack-local');
+
 exports.config = {
   allScriptsTimeout: 11000,
+  'seleniumAddress': 'http://hub-cloud.browserstack.com/wd/hub',
+  'capabilities': {
+    'browserstack.user': 'michaelmorris5',
+    'browserstack.key': 'CnK3B8wAxF4xpsu54xJc',
+    'browserstack.local': true,
+    'browserName': 'chrome'
+  },
   specs: [
     './e2e/**/*.e2e-spec.ts'
   ],
-  capabilities: {
-    'browserName': 'chrome'
-  },
-  directConnect: true,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
@@ -24,5 +30,25 @@ exports.config = {
       project: 'e2e/tsconfig.e2e.json'
     });
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+  },
+    // Code to start browserstack local before start of test
+  beforeLaunch: function(){
+    console.log("Connecting local");
+    return new Promise(function(resolve, reject){
+      exports.bs_local = new browserstack.Local();
+      exports.bs_local.start({'key': exports.config.capabilities['browserstack.key'] }, function(error) {
+        if (error) return reject(error);
+        console.log('Connected. Now testing...');
+
+        resolve();
+      });
+    });
+  },
+
+  // Code to stop browserstack local after end of test
+  afterLaunch: function(){
+    return new Promise(function(resolve, reject){
+      exports.bs_local.stop(resolve);
+    });
   }
 };
